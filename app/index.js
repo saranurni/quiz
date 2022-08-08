c8d6fb7ef43627c2e3c37d71f239ae9e121611d5
Sara Nurestani <saranurestani@gmail.com>
2022-08-09 00:11:52 +0100

    Change to index and added commas

--- app/index.js
+++ app/index.js
@@ -1,1 +1,34 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+const customer = {
+  firstName:"Sara",
+  lastName:"Nurestani",
+  id:1234,
+  phoneNumber:"0712341234",
+  email:"saranurestani@gmail.com"}
+
+customer = {
+  street:"street lane",
+  city:"london",
+  state:"middlesex",
+  zipCode:"123456"}
+
+console.log(`Welcome, ${customer.firstName} ${customer.lastName}!`)
+
+const product = {name:"sara"; price:12; description:"nice"; quantity:34}
+
+console.log(`Total: %<total> ${price*quantity}`.)
+
+
+
+
+
+
+// arrays
+
+const nums = [2, 4, 6, 8, 10];
+
+const num = nums[6];
+console.log(num);
+
+const colors = ["red", "green", "blue"];
+const col = colors[0];
+console.log(col);
