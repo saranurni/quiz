e9ceeca1fa823af9cb51ce0cdfef810816646ef4
Sara Nurestani <saranurestani@gmail.com>
2022-08-09 11:32:10 +0100

    Quiz finished

--- app/index.js
+++ app/index.js
@@ -1,34 +1,30 @@
 const customer = {
-  firstName:"Sara",
-  lastName:"Nurestani",
-  id:1234,
-  phoneNumber:"0712341234",
-  email:"saranurestani@gmail.com"}
+  firstName: "Sara",
+  lastName: "Nurestani",
+  id: 1234,
+  phoneNumber: "0712341234",
+  email: "saranurestani@gmail.com",
+};
 
-customer = {
-  street:"street lane",
-  city:"london",
-  state:"middlesex",
-  zipCode:"123456"}
+customer.address = {
+  street: "street lane",
+  city: "london",
+  state: "middlesex",
+  zipCode: "123456",
+};
 
-console.log(`Welcome, ${customer.firstName} ${customer.lastName}!`)
+console.log(`Welcome, ${customer.firstName} ${customer.lastName}!`);
 
-const product = {name:"sara"; price:12; description:"nice"; quantity:34}
+const product = {
+  name: "Sara",
+  price: 12,
+  description: "nice",
+  quantity: 34,
+};
 
-console.log(`Total: %<total> ${price*quantity}`.)
+const total = product.price * 34;
 
+console.log(total >= 100);
 
-
-
-
-
-// arrays
-
-const nums = [2, 4, 6, 8, 10];
-
-const num = nums[6];
-console.log(num);
-
-const colors = ["red", "green", "blue"];
-const col = colors[0];
-console.log(col);
+product.quantity -= 7;
+product.quantity -= 7;
